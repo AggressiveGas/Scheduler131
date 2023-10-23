10790209b60b9d1a6d42d0ac7143eedb8aa9652a
RicardoTTorres <ricardotorres@csus.edu>
2023-10-22 20:55:21 -0700

    Some Navigation to move between pages

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,41 @@
 import React, { useState } from "react";
 import { AppBar, Box, Tabs, Tab, Toolbar, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom"; // Import useLocation from react-router-dom
 
 const Header = () => {
+    const location = useLocation();
+    const shouldShowHeader = !location.pathname.startsWith("/welcome"); // Conditionally decide whether to show the header
+
     const [value, setValue] = useState();
 
     return (
         <div>
-            <AppBar position="sticky">
-                <Toolbar>
-                    <Typography variant="h3">
-                        Scheduler131
-                        <Typography
-                            variant="body2"
-                            component="span"
-                            sx={{ color: '#00FF00' }} // Apply a bright green color
-                        >
-                            (Not official UI; basic layout just so we can work on front-end back-end calls)
+            {shouldShowHeader && ( // Conditionally render the header
+                <AppBar position="sticky">
+                    <Toolbar>
+                        <Typography variant="h3">
+                            Scheduler131
+                            <Typography
+                                variant="body2"
+                                component="span"
+                                sx={{ color: '#00FF00' }} // Apply a bright green color
+                            >
+                                (Not official UI; basic layout just so we can work on front-end back-end calls)
+                            </Typography>
                         </Typography>
-                    </Typography>
-                    <Box sx={{ marginLeft: "auto" }}>
-                        <Tabs indicatorColor="secondary"
-                            onChange={(e, val) => setValue(val)}
-                            value={value}
-                            textColor="inherit">
-                            <Tab to="/login" LinkComponent={Link} label="Login" />
-                            <Tab to="/register" LinkComponent={Link} label="Register" />
-                        </Tabs>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+                        <Box sx={{ marginLeft: "auto" }}>
+                            <Tabs indicatorColor="secondary"
+                                onChange={(e, val) => setValue(val)}
+                                value={value}
+                                textColor="inherit">
+                                <Tab to="/login" LinkComponent={Link} label="Login" />
+                                <Tab to="/register" LinkComponent={Link} label="Register" />
+                            </Tabs>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+            )}
         </div>
     )
 }
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mui/material';
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
-    return(
+    return (
         <div style={{ padding: '100px 20px', backgroundColor: 'green' }}>
-        WELCOME USER
-    </div>
-    )
+            <div>WELCOME USER</div>
+            <Button
+                component={Link}
+                to="/"
+                variant="outlined"
+                color="primary"
+                sx={{ backgroundColor: 'blue', color: 'white' }}
+            >
+                Return to Home
+            </Button>
+        </div>
+    );
 }
 
 export default Welcome;
