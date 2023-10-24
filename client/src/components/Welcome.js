0827c03d01a63a494fd128cf28cd788ccde31d46
RicardoTTorres <ricardotorres@csus.edu>
2023-10-24 15:44:32 -0700

    Added DeleteUser, No Token As of Now

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+
+//to delete a user from the database
+const handleDeleteUser = async () => {
+    try {
+        const response = await axios.delete("http://localhost:8080/api/user/(PutID)"); //use real ID in (), get from mongoDB --I dont have access to the DB as of rn
+        console.log("User has been deleted");
+    } catch (error) {
+        console.error("Error deleting user: ", error);
+    }
+};
+
 
 const Welcome = () => {
     return (
@@ -14,6 +26,15 @@ const Welcome = () => {
                 sx={{ backgroundColor: 'blue', color: 'white' }}
             >
                 Return to Home
+            </Button>
+            <div></div>
+            <Button
+                variant="outlined"
+                color="secondary"
+                onClick={handleDeleteUser}
+                sx={{ marginTop: '20px', backgroundColor: 'red', color: 'white' }}
+            >
+                Delete User
             </Button>
         </div>
     );
