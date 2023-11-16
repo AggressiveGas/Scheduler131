0334cf003ff06204940653d76573ba914aa08744
RicardoTTorres <ricardotorres@csus.edu>
2023-11-15 18:43:25 -0800

    Fixed Routing +  remove  unecessary code/comment

--- main/src/Header.js
+++ main/src/Header.js
@@ -219,11 +219,6 @@ export default function Header() {
           {/*Divides out the navbar*/}
           <div class = "border-l border-slate-200 ml-6 pl-6 dark:border-slate-800"></div>
          
-          {/*User settings*/}
-          <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">User settings <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-          </svg>
-          </button>
           {/*Dropdown menu*/}
           <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
               <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownButton">
--- main/src/util/Home.js
+++ main/src/util/Home.js
@@ -100,39 +100,3 @@ export default function Home() {
 	</div>
 	);
 }
-
-
-
-/*
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}*/
-
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
