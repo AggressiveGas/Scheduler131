2830d108a07e6e26b1bd903ce98455aac7d82100
williamlorence <williamlorence@csus.edu>
2023-12-04 14:53:11 -0800

    added full crud for meetings, let me know if changes are needed

--- server/controllers/meetingcontroller.js
+++ server/controllers/meetingcontroller.js
@@ -4,8 +4,8 @@ const Meeting = require('../models/meetingmodel')
 const mongoose = require('mongoose')
 
 // @desc Create a new meeting
-// @route POST /api/rooms/joincode/meetings
-// @access Private
+// @route POST /api/meetings/:joincode
+// @access Public
 const createMeeting = asyncHandler(async (req, res) => {
     const room = await Room.findOne({joincode: req.params.joincode})
     if(!room){      //Checks if param joincode is an existing room
@@ -29,6 +29,56 @@ const createMeeting = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc Get existing meetings for a room
+// @route GET /api/meetings/:joincode
+// @access Public
+const getMeetings = asyncHandler(async (req, res) => {
+    const meetings = await Meeting.find({roomcode: req.params.joincode})
+
+    if(!meetings){
+        res.status(404)
+        throw new Error('No rooms found')
+    }
+
+    res.status(200).json({meetings})
+})
+
+// @desc Edit a single existing meeting
+// @route PUT /api/meetings/:id
+// @access Public
+const editMeetingTime = asyncHandler(async (req, res) => {
+    const meeting = await Meeting.findById(req.params.id)
+
+    if(!meeting){  // if the meeting does not exist, throw an error at status 404
+        res.status(404)
+        throw new Error('Meeting not found')
+    }
+
+    const updatedMeeting = await Meeting.findByIdAndUpdate(req.params.id, req.body, { // updates the meeting time
+        new: true,
+        runValidators: true
+    })
+    
+    res.status(200).json(updatedMeeting) // sends the updated meeting data back
+})
+
+// @desc Delete a meeting
+// @route DELETE /api/meetings/:id
+// @access Public
+const deleteMeeting = asyncHandler(async (req, res) => {
+    const meeting = await Meeting.findById(req.params.id)   //meeting object id in request
+
+    if(!meeting){  // if the meeting does not exist, throw an error at status 404
+        res.status(404)
+        throw new Error('Meeting not found')
+    }
+
+    const deletedMeeting = await Meeting.findByIdAndDelete(req.params.id) // deletes the user data
+
+    res.status(200).json({deletedMeeting}).select('_id') // sends the deleted user id back
+
+})
+
 module.exports = {  // export the functions
-    createMeeting, 
+    createMeeting, deleteMeeting, getMeetings, editMeetingTime
 }   
--- server/routes/meetingroutes.js
+++ server/routes/meetingroutes.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const {createMeeting} = require('../controllers/meetingcontroller') // import the functions from the controller
+const {createMeeting, deleteMeeting, getMeetings, editMeetingTime} = require('../controllers/meetingcontroller') // import the functions from the controller
 const {protect} = require('../middlewares/authmiddleware')  // import the middleware
 
 
 
 // create routes
-router.post('/:joincode',createMeeting) // this is the route for creating a room
+router.post('/:joincode',createMeeting) // this is the route for creating a meeting
+
+//read routes
+router.get('/:joincode',getMeetings) // reading all meetings for a room
+
+//update routes
+router.put('/:id',editMeetingTime) //update a meeting's timeslot
+
+//delete routes
+router.delete('/:id',deleteMeeting)   //deleting meeting
 
 module.exports = router; // export the router
