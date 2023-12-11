4f2642709d210ad43c4b2cc445d0d6425563e361
Ajaydeep Singh <deepajay0713@gmail.com>
2023-12-10 17:38:58 -0800

    Time Picker executed. handleDateChange + handleTimeChange implemented
    Hope it works!

--- main/src/util/Room.js
+++ main/src/util/Room.js
@@ -6,10 +6,15 @@ import './RoomStyling.css';
 import DateTimePicker from 'react-tailwindcss-datetimepicker';
 import moment from 'moment-timezone';
 import { Input, Popover, PopoverHandler, PopoverContent} from "@material-tailwind/react";
-import { format } from "date-fns";
+import { format, addHours } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { Timepicker, initTE } from "tw-elements";
+import TimePicker from 'react-time-picker';
+
+
+
+
 
 
 
@@ -34,11 +39,6 @@ const Room = () => {
   const [from, setFrom] = useState("0");
   const [to, setTo] = useState("0");
 
-  //trying to split the date so that it can later be used for comparisons
-  const SplitDate = (defaultValue) => {
-    const arrayDate = defaultValue.toLocaleDateString().split("/");
-    console.log(defaultValue);
-  }
   
   const handleAddClick = () => {
     setShowPopup(true);
@@ -176,7 +176,7 @@ const fetchUserDetails = async (userId) => {
   }
 };
 
-//for the DATETIMEPICKER UI
+{/* For the DATETIMEPICKER UI. Delete when availabilities are resolved
 const now = new Date();
   const start = moment(
     new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
@@ -195,10 +195,68 @@ const now = new Date();
     sundayFirst: false,
   };
   const maxDate = moment(start).add(30, 'month');
-
   function handleApply(startDate, endDate) {
     setRange({ start: startDate, end: endDate });
   }
+  */}
+
+  //These are being used for the "Select your availability" input field. 
+  const startDate = date ? format(date, "MM/dd/yyyy,") : "Select a Date";
+  //const startTime = date ? format(date, "h:mm a") : "";
+  //const endTime = date ? format(addHours(date, 1), "h:mm a") : "";
+  //const value = date ? `${startDate} ${startTime} to ${endTime}` : "Select a Date and Time";
+  
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [selectedStartTime, setSelectedStartTime] = useState(null);
+  const [selectedEndTime, setSelectedEndTime] = useState(null);
+
+  const updateDate = () => {
+    setSelectedDate(date);
+    if (date) {
+      const day = date.getDate();
+      const month = date.getMonth() + 1; // Months are zero-indexed, so we add 1
+      setSelectedDay(day);
+      setSelectedMonth(month);
+    } else {
+      setSelectedDay(null);
+      setSelectedMonth(null);
+    }
+  };
+  
+  const handleDateChange = (date) => {
+    updateDate(date);
+  };
+
+  // In progress: Split the date so that it can later be used for comparisons
+  const SplitDate = (defaultValue) => {
+    const arrayDate = defaultValue.toLocaleDateString().split("/");
+    console.log(defaultValue);
+  }
+
+
+  //for display on the input field
+  const [startTime, setStartTime] = useState({ hours: '1', minutes: '0', ampm: 'am' });
+  const [endTime, setEndTime] = useState({ hours: '1', minutes: '0', ampm: 'am' });
+
+
+  const handleTimeChange = (event, timeType, part) => {
+    const selectedTime = { ...timeType, [part]: event.target.value };
+    timeType === 'start' ? setStartTime(selectedTime) : setEndTime(selectedTime);
+  };
+
+  const formatTime = (time) => (time ? `${time.hours}:${time.minutes} ${time.ampm}` : '');
+
+  const value = date ? `${startDate} ${formatTime(startTime)} to ${formatTime(endTime)}`
+  : 'Select a Date and Time';
+
+  const showAlert = () => {
+    const message = `You have selected: ${selectedMonth}/${selectedDay} from ${
+      selectedStartTime} to ${selectedEndTime}`;
+    alert(message);
+  };
+
 
 
 //everything below is formatting/buttons/inputs for the pop up forms.
@@ -266,8 +324,8 @@ const now = new Date();
                 DateTimePicker UI
               </a>
             </DateTimePicker>
-
             */}
+
             <a href="#" onClick={handleAvailability} 
                 className="block rounded-lg px-3 py-2 mt-2 text-sm font-semibold leading-6 text-gray-900 bg-gray-100 hover:bg-gray-300"
             >  
@@ -411,26 +469,18 @@ const now = new Date();
               <span className="text-2xl font-bold">&larr;	</span>
             </button>
 
-            <p className="text-lg font-semibold mb-4">Select your Availability</p>
+            <p className="text-lg font-semibold mb-4">Select your availability</p>
 
-            
-
-
-            
-
-
-
-
-
-
+            {/* Availability */}
             <div className="p-26">
               <Popover placement="right">
                 <PopoverHandler>
-                  <Input
-                    onChange={() => null}
-                    value={date ? format(date, "PPP") : "Select a Date and Time"}
-                    className = "rounded-lg"
-                  />
+                <Input
+                  onChange={() => null}
+                  type = "text"
+                  value={value}
+                  className="rounded-lg mr-56 pl-13 pr-0"
+                />
                 </PopoverHandler>
                 <PopoverContent>
                   
@@ -479,37 +529,103 @@ const now = new Date();
                     }}
                   
                   />
-
+                  <p>Selected Day: {selectedDay}</p>
+                  <p>Selected Month: {selectedMonth}</p>
                   <hr className= "mt-4"></hr>
-
-                  {/*Time picker*/}
-                  <div className = "mt-4 flex justify-center">
-                    <select id="from" className = "p-2 border border-gray-300" onChange={t=> setFrom(t)} >
-                      <option value="0">Select Time</option>
-                      <option value="12">12 PM</option>
-                      .
-                      .
-                      . 
-                      <option value="11">11 AM</option>
-                    </select>
-                    <b className= "p-2">TO</b>
-
-                    <select id="toDate" className = "p-2 border border-gray-300" onChange={t=> setTo(t)} >
-                      <option value="0">Select Time</option>
-                      <option value="12">12 PM</option>
-                      .
-                      .
-                      . 
-                      <option value="11">11 AM</option>
-                    </select>
+                  
+                  {/* Time picker component from react-time-picker. Currently not in use.
+                  <div>
+                    <h2>Select a Time:</h2>
+                    <TimePicker
+                      onChange={handleTimeChange}
+                      value={selectedTime}
+                      format="h:mm a"
+                      disableClock={true} // Optional: You can customize the appearance further
+                      clearIcon={null}    // Optional: Remove clear icon
+                      classNames = "border border-gray-300"
+                    />
+                    <p>Selected Time: {selectedTime}</p>
                   </div>
+                  */}
+
+
+                  {/* Time picker */}
+                  <div class="mt-4 flex justify-center">
+                    <div class="flex p-2 border border-gray-300 rounded-lg">
+
+                      {/* The selected value is associated with the "name" attribute */}
+                      <select name="hours" class="bg-transparent appearance-none outline-none" 
+                        type="number"
+                        value={startTime.hours}
+                        onChange={(e) => handleTimeChange(e, 'start', 'hours')}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">10</option>
+                        <option value="12">12</option>
+                      </select>
+                      <span class=" mr-3">:</span>
+                      <select name="minutes" class="bg-transparent appearance-none outline-none mr-4" type="number" value={startTime.minutes}
+                        onChange={(e) => handleTimeChange(e, 'start', 'minutes')}>
+                        <option value="0">00</option>
+                        <option value="30">30</option>
+                      </select>
+                      <select name="ampm" class="bg-transparent appearance-none outline-none" value={startTime.ampm}
+                        onChange={(e) => handleTimeChange(e, 'start', 'ampm')}>
+                        <option value="am">AM</option>
+                        <option value="pm">PM</option>
+                      </select>
+                    </div>
+                    <b className= "p-2">TO</b>
+                    <div class="flex p-2 border border-gray-300 rounded-lg">
+                      <select name="hours" class="bg-transparent appearance-none outline-none" type="number" value={endTime.hours}
+                        onChange={(e) => handleTimeChange(e, 'end', 'hours')}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">10</option>
+                        <option value="12">12</option>
+                      </select>
+                      <span class=" mr-3">:</span>
+                      <select name="minutes" class="bg-transparent appearance-none outline-none mr-4" type="number" value={endTime.minutes}
+                        onChange={(e) => handleTimeChange(e, 'end', 'minutes')}>
+                        <option value="0">00</option>
+                        <option value="30">30</option>
+                      </select>
+                      <select name="ampm" class="bg-transparent appearance-none outline-none" value={endTime.ampm}
+                        onChange={(e) => handleTimeChange(e, 'end', 'ampm')}>
+                        <option value="am">AM</option>
+                        <option value="pm">PM</option>
+                      </select>
+                    </div>
+
+                  </div>
+
+                 
                   
 
                   
                          
                   <button 
                     className="bg-red-500 hover:bg-red-700 text-white px-10 py-2 rounded-lg mt-6 mb-2 ml-20"
-                    onClick={() => SplitDate(setDate)}>
+                    onClick={() => {
+                      handleDateChange(setDate);
+                      showAlert();
+                    }}>
                       Apply
                   </button>
                   
