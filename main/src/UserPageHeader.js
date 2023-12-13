b8ba4b5418d407a056713d593731e5747c63d2fa
RicardoTTorres <ricardotorres@csus.edu>
2023-12-12 23:54:21 -0800

    Dropdown header should be read for everyone now

--- main/src/UserPageHeader.js
+++ main/src/UserPageHeader.js
@@ -285,7 +285,7 @@ export default function UserPageHeader() {
             <button
               id="dropdownDividerButton"
               data-dropdown-toggle="dropdownDivider"
-              className="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
               type="button"
               onClick={toggleDropdown}
             >
